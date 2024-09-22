b0b2a92bb8f315a695aae5299d318e4adab9be19
anahi <luna.calderon@mi.unc.edu.ar>
2024-09-22 16:57:51 -0300

    Instanciando canciones y podcast

---
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -0,0 +1,26 @@
+package com.aluracursos.radioalura.principal;
+
+import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Cancion miCancion = new Cancion();
+        miCancion.setTitulo("Forever");
+        miCancion.setCantante("Kiss");
+
+        Podcast miPodcast = new Podcast();
+        miPodcast.setPresentador("Gabriela Aguilar");
+        miPodcast.setTitulo("Cafe.Tech");
+
+        for (int i = 0; i < 100 ; i++) {
+            miCancion.meGusta();
+        }
+        for (int i = 0; i < 5000; i++) {
+            miCancion.reproduce();
+        }
+        System.out.println("Total reproducciones: "+miCancion.getTotalDeReproducciones());
+        System.out.println("Total de me gusta: " + miCancion.getTotalDeMeGusta());
+    }
+
+}
